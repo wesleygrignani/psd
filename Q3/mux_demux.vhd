abfdc16c2a35544fdda2fdb8f5ccca0bf7300308
wesleygrignani <wesley.grignani@edu.univali.br>
2023-09-12 10:40:25 -0300

    Update demux and mux_demux

--- Q3/mux_demux.vhd
+++ Q3/mux_demux.vhd
@@ -8,8 +8,7 @@ entity mux_demux is
     i_SEL_M : in std_logic; -- mux selector
     i_SEL_D : in std_logic; -- demux selector
     o_S0    : out std_logic; -- data output 
-    o_S1    : out std_logic; -- data output
-  );
+    o_S1    : out std_logic); -- data output
 end mux_demux;
 
 architecture rtl of mux_demux is
@@ -20,8 +19,7 @@ architecture rtl of mux_demux is
       i_SEL : in std_logic;
       i_A   : in std_logic;
       i_B   : in std_logic;
-      o_S   : out std_logic
-    );
+      o_S   : out std_logic);
   end component;
 
   -- demux component
@@ -30,8 +28,7 @@ architecture rtl of mux_demux is
       i_SEL : in std_logic;
       i_D   : in std_logic;
       o_S0  : out std_logic;
-      o_S1  : out std_logic
-    );
+      o_S1  : out std_logic);
   end component;
 
   signal w_D : std_logic := '0'; -- signal to connect mux output to demux input
@@ -43,15 +40,13 @@ begin
     i_SEL => i_SEL_M,
     i_A   => i_A,
     i_B   => i_B,
-    o_S   => w_D
-  );
+    o_S   => w_D);
 
   demux : demux1_2bit
   port map(
     i_SEL => i_SEL_D,
     i_D   => w_D,
     o_S0  => o_S0,
-    o_S1  => o_S1
-  );
+    o_S1  => o_S1);
 
 end rtl;
