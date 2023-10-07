0bdd2b30f18ff3e6c4e791485ca770bd2b218c5b
MichaelDouglas <michaeld@edu.univali.br>
2023-10-07 15:43:05 -0300

    Questão 2 resolvido

--- T2/Q2/fsm_car.vhd
+++ T2/Q2/fsm_car.vhd
@@ -5,11 +5,12 @@ entity fsm_car is
   port (
     i_CLK : in std_logic; -- clock
     i_RST : in std_logic; -- reset/clear
-    o_Z   : out std_logic_vector(3 downto 0)); -- data output
+    i_A : in std_logic; -- key car
+    o_R   : out std_logic); -- data output
 end fsm_car;
 
 architecture arch_1 of fsm_car is
-  type t_STATE is (s_A, s_B, s_C, s_D);
+  type t_STATE is (s_K1, s_K2, s_K3, s_K4, s_Wait);
   signal w_NEXT  : t_STATE; -- next state
   signal r_STATE : t_STATE; -- current state
 
@@ -18,7 +19,7 @@ begin
   process (i_RST, i_CLK)
   begin
     if (i_RST = '1') then
-      r_STATE <= s_A;
+      r_STATE <= s_Wait;
     elsif (rising_edge(i_CLK)) then
       r_STATE <= w_NEXT;
     end if;
@@ -28,18 +29,20 @@ begin
   process (r_STATE)
   begin
     case r_STATE is
-      when s_A    => w_NEXT    <= s_B;
-      when s_B    => w_NEXT    <= s_C;
-      when s_C    => w_NEXT    <= s_D;
-      when s_D    => w_NEXT    <= s_A;
-      when others => w_NEXT <= s_A;
+      when s_Wait  => if(i_A = '1') then 
+      w_NEXT    <= s_K1;
+      else
+      w_NEXT    <= s_Wait;        
+      end if;
+      when s_K1    => w_NEXT    <= s_K2;
+      when s_K2    => w_NEXT    <= s_K3;
+      when s_K3    => w_NEXT    <= s_K4;
+      when s_K4    => w_NEXT    <= s_Wait;
+      when others => w_NEXT <= s_Wait;
     end case;
   end process;
 
   -- Output Function
-  o_Z <= "0001" when (r_STATE = s_A) else
-    "0011" when (r_STATE = s_B) else
-    "1100" when (r_STATE = s_C) else
-    "1000";
+  o_R <= '1' when (r_STATE = s_K1 and r_STATE = s_K2 and r_STATE = s_K4) else '0';
 
 end arch_1;
