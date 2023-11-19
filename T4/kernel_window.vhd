680d431e8cf21ea6acaf3f577bc19ff34b229d4a
wesleygrignani <wesley.grignani@edu.univali.br>
2023-11-19 16:58:20 -0300

    update

--- T4/kernel_window.vhd
+++ T4/kernel_window.vhd
@@ -1,24 +1,22 @@
--- Nome: Wesley Grignani
--- Desc: Line buffer para buferizar uma linha da imagem
-
 library ieee;
 use ieee.std_logic_1164.all;
 library work;
 use work.filter_pkg.all;
 
-entity line_buffer is
+entity kernel_window is
   port (
     i_clk   : in std_logic;
     i_ld    : in std_logic;
     i_rst   : in std_logic;
     i_pixel : in std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-    o_pixel : out std_logic_vector(PIXEL_WIDTH - 1 downto 0));
+    o_pixel : out std_logic_vector(PIXEL_WIDTH - 1 downto 0)
+  );
 end entity;
 
-architecture rtl of line_buffer is
+architecture rtl of kernel_window is
 
-  type t_linebuffer is array(0 to IMG_WIDTH - KERNEL_SIZE) of std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal r_linebuffer : t_linebuffer;
+  type t_kernel_window is array(0 to KERNEL_SIZE) of std_logic_vector(PIXEL_WIDTH - 1 downto 0);
+  signal r_kernel_window : t_kernel_window;
 
   -- registrador
   component reg
@@ -46,10 +44,10 @@ begin
     i_ena => i_ld,
     i_rst => i_rst,
     i_d   => i_pixel,
-    o_q   => r_linebuffer(0)
+    o_q   => r_kernel_window(0)
   );
 
-  gen_linebuffer : for i in 1 to IMG_WIDTH - KERNEL_SIZE - 1 generate
+  gen_linebuffer : for i in 1 to KERNEL_SIZE - 1 generate
     reg_linebuffer : reg
     generic map(
       p_WIDTH => PIXEL_WIDTH
@@ -58,11 +56,11 @@ begin
       i_clk => i_clk,
       i_ena => i_ld,
       i_rst => i_rst,
-      i_d   => r_linebuffer(i - 1),
-      o_q   => r_linebuffer(i)
+      i_d   => r_kernel_window(i - 1),
+      o_q   => r_kernel_window(i)
     );
   end generate;
 
-  o_pixel <= r_linebuffer(IMG_WIDTH - KERNEL_SIZE - 1);
+  o_pixel <= r_kernel_window(KERNEL_SIZE - 1);
 
 end architecture;
