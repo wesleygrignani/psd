7bcb9eddf9bf46f99ca9bb8ae1a8853da7aba3f8
wesleygrignani <wesley.grignani@edu.univali.br>
2023-12-02 13:30:59 -0300

    update testbench

--- T4/filter_tb.vhd
+++ T4/filter_tb.vhd
@@ -4,46 +4,28 @@ use IEEE.NUMERIC_STD.all;
 library work;
 use work.filter_pkg.all;
 
-entity img_buffer_tb is
+entity filter_tb is
 end entity;
 
-architecture rtl of img_buffer_tb is
+architecture rtl of filter_tb is
 
-  component img_buffer
+  component filter_top
     port (
-      i_clk           : in std_logic;
-      i_ld            : in std_logic;
-      i_rst           : in std_logic;
-      i_pixel         : in std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel1 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel2 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel3 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel4 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel5 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel6 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel7 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel8 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-      o_kernel_pixel9 : out std_logic_vector(PIXEL_WIDTH - 1 downto 0)
+      i_clk    : in std_logic;
+      i_rst    : in std_logic;
+      i_start  : in std_logic;
+      i_pixel  : in std_logic_vector(PIXEL_WIDTH - 1 downto 0);
+      o_filter : out std_logic_vector(PIXEL_WIDTH - 1 downto 0)
     );
   end component;
 
   constant c_CLK_PERIOD : time := 10 ns;
 
-  signal w_clk   : std_logic;
-  signal w_ld    : std_logic;
-  signal w_rst   : std_logic;
-  signal w_pixel : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-
-  -- output signals from kernel
-  signal w_kernel1 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel2 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel3 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel4 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel5 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel6 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel7 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel8 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
-  signal w_kernel9 : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
+  signal w_clk    : std_logic;
+  signal w_rst    : std_logic;
+  signal w_start  : std_logic;
+  signal w_pixel  : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
+  signal w_filter : std_logic_vector(PIXEL_WIDTH - 1 downto 0);
 
 begin
 
@@ -56,31 +38,23 @@ begin
     wait for c_CLK_PERIOD/2;
   end process;
 
-  img_buffer_inst : img_buffer
+  filter_top_inst : filter_top
   port map(
-    i_clk           => w_clk,
-    i_ld            => w_ld,
-    i_rst           => w_rst,
-    i_pixel         => w_pixel,
-    o_kernel_pixel1 => w_kernel1,
-    o_kernel_pixel2 => w_kernel2,
-    o_kernel_pixel3 => w_kernel3,
-    o_kernel_pixel4 => w_kernel4,
-    o_kernel_pixel5 => w_kernel5,
-    o_kernel_pixel6 => w_kernel6,
-    o_kernel_pixel7 => w_kernel7,
-    o_kernel_pixel8 => w_kernel8,
-    o_kernel_pixel9 => w_kernel9
+    i_clk    => w_clk,
+    i_rst    => w_rst,
+    i_start  => w_start,
+    i_pixel  => w_pixel,
+    o_filter => w_filter
   );
   process
   begin
     w_rst   <= '1';
-    w_ld    <= '0';
+    w_start <= '0';
     w_pixel <= "00000000";
     wait for c_CLK_PERIOD;
 
     w_rst   <= '0';
-    w_ld    <= '1';
+    w_start <= '1';
     w_pixel <= std_logic_vector(to_unsigned(1, PIXEL_WIDTH));
     wait for c_CLK_PERIOD;
 
@@ -152,5 +126,42 @@ begin
 
     w_pixel <= std_logic_vector(to_unsigned(24, PIXEL_WIDTH));
     wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(25, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(26, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(27, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(28, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(29, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(30, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(31, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(32, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(33, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(34, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(35, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
+    w_pixel <= std_logic_vector(to_unsigned(36, PIXEL_WIDTH));
+    wait for c_CLK_PERIOD;
+
   end process;
 end architecture;
